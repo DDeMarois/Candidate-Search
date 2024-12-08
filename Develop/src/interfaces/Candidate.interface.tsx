fc6c26d0a6acff826c641f063fb9281e9a48f001
DDeMarois <Drew.DeMarois@du.edu>
2024-12-08 16:59:57 -0700

    refactor: update Candidate interface to enforce required fields and maintain consistency

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,12 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 interface Candidate {
     id: number;
+    avatar_url: string;
     name: string;
     login: string;
-    location?: string;
-    email?: string;
-    company?: string;
-    avatar_url: string;
+    location: string;
+    email: string;
+    company: string;
     html_url: string;
-}
-export default Candidate;
+} 
+    export default Candidate;
